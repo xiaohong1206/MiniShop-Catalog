2a15764ff7fcd8a3045057dba08b84861fb659a8
xiaohong1206 <thuyhuong12062k4@gmail.com>
2026-07-24 15:31:38 +0700

    Create database.php

---
+++ cse485-ms-05/config/database.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+function db(): PDO
+{
+    static $pdo = null;
+    if ($pdo instanceof PDO) {
+        return $pdo;
+    }
+
+    $pdo = new PDO(
+        'mysql:host=127.0.0.1;dbname=minishop_cse485;charset=utf8mb4',
+        'root',
+        '',
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ]
+    );
+
+    return $pdo;
+}
